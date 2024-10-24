88e61b3791f37f2de102998d28ea179905eb3c3d
priscilazarebelni <160619289+priscilazarebelni@users.noreply.github.com>
2024-10-24 11:35:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Ciências',
+    'Qual é o estado fíico da água a 0°C?',
+    'Sólido (gelo)'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro a Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em território?',
+    'Rússia'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'Newton (N)'
+)
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    'H₂O'
+)
+criaCartao(
+    'Matemática',
+    'Quanto é 8x7?',
+    '56'
+)
